694619f99205d405274e28e38629d41463c94e9a
madan-zz <madan.innovative@gmail.com>
2020-02-04 18:01:06 +0530

    4 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { TopMenuComponent } from './top_menu/top.menu.component';
+import { ProductComponent } from './product-list/product.component';
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, TopMenuComponent ],
+  imports:      [ BrowserModule, FormsModule,  RouterModule],
+  declarations: [ AppComponent, HelloComponent, TopMenuComponent, ProductComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
