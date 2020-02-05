f7a9efcf3b056720967da556298460a8cefd6f72
madan-zz <madan.innovative@gmail.com>
2020-02-05 17:48:12 +0530

    4 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { TopMenuComponent } from './top_menu/top.menu.component';
 import { ProductComponent } from './product-list/product.component';
+import { ProductAlertComponent } from './product-alert/product-alert/product-alert.component';
 @NgModule({
   imports:      [ BrowserModule, FormsModule,  RouterModule],
-  declarations: [ AppComponent, HelloComponent, TopMenuComponent, ProductComponent ],
+  declarations: [ AppComponent, HelloComponent, TopMenuComponent, ProductComponent, ProductAlertComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/product-list/product.component.ts
+++ src/app/product-list/product.component.ts
@@ -10,8 +10,11 @@ import { products } from '../products';
 export class ProductComponent {
  products = products;
 
-  share() {
-    window.alert('The product has been shared!');
+  share(name) {
+    window.alert(name+'The product has been shared!');
+  }
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
   }
 }
 
