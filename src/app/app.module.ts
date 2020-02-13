d312bc53b5c1ad0c1ff5daa679fc0fc762e7924b
madan-zz <madan.innovative@gmail.com>
2020-02-13 17:06:22 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,12 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
 import { CartComponent } from './cart/cart.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,  RouterModule.forRoot([
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot([
       { path: '', component: ProductComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
-      {path: '/cart', component: CartComponent}
+      {path: 'cart', component: CartComponent}
     ])],
-  declarations: [ AppComponent, HelloComponent, TopMenuComponent, ProductComponent, ProductAlertComponent, ProductDetailsComponent ],
+  declarations: [ AppComponent, HelloComponent, TopMenuComponent, ProductComponent, ProductAlertComponent, ProductDetailsComponent, CartComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
